c25b7e7e86f4dc5db0dac3aeae761b4d4bb1db18
haoziapple <haozixiaowang@163.com>
2022-05-04 14:14:57 +0800

    add magit

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(clojure-mode-extra-font-locking cider)))
+ '(package-selected-packages '(magit clojure-mode-extra-font-locking cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -95,3 +95,7 @@
 	     :ensure t
 	     :init
 	     (load-theme 'dracula t))
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
