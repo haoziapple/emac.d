2d8cd9b09600e4a3e743ff386fdfaac6b50cde89
haoziapple <haozixiaowang@163.com>
2022-08-08 11:09:09 +0800

    add doom-themes and doom-modeline

--- init.el
+++ init.el
@@ -30,11 +30,13 @@
 (require 'package)
 
 (add-to-list 'package-archives             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;;(add-to-list 'package-archives             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 (add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(doom-modeline . "melpa-stable") t)
 
 
 ;; Load and activate emacs packages. Do this first so that the
@@ -69,7 +71,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit clojure-mode-extra-font-locking cider)))
+ '(package-selected-packages
+   '(doom-modeline doom-themes all-the-icons magit clojure-mode-extra-font-locking cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -90,7 +93,7 @@
 (global-set-key (kbd "<f2>") 'open-init-file)
 
 ;; leuven,tango-dark,deeper-blue,wheatgrass
-(load-theme 'wheatgrass t)
+;;(load-theme 'wheatgrass t)
 ;; (use-package dracula-theme
 ;; 	     :ensure t
 ;; 	     :init
@@ -99,3 +102,31 @@
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
+
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons
+  :ensure t)
+
+;;emacs themes
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;;doom-modeline
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
